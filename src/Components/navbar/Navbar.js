397e3b0a07c01d8ef01a3baa963284f9526bfc00
kacper <k4cper.petelicki@gmail.com>
2024-02-24 23:17:36 +0100

    add sccs

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./navbar.scss";
-import { Notifications, Search } from '@material-ui/icons';
+import { ArrowDropDown, Notifications, Search } from '@material-ui/icons';
 export default function Navbar() {
   return (
     <div className='navbar'>
@@ -20,7 +20,12 @@ export default function Navbar() {
       <Search/>
       <span>KID</span>
       <Notifications/> 
-      
+      <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+      <ArrowDropDown/>
+
     </div>
     </div>
     </div>
