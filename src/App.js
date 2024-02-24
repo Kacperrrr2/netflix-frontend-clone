bb1a6e5875cce2215743043da46fd28dbf4f3499
kacper <k4cper.petelicki@gmail.com>
2024-02-24 22:42:43 +0100

    add Navbar.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './home/home';
+import "./App.scss";
+function App() {
+  return (
+    <div><Home/></div>
+  );
+}
+
+export default App;
---
+++ src/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./home.scss";
+import Navbar from '../Components/navbar/Navbar';
+export default function home() {
+  return (
+    <div className='home'>
+        <Navbar/>
+    </div>
+  )
+}
+
+  
+
